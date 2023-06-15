f26c07c23d0e4f9d908e5ae9dfa472271582f6ef
aakamenov <asparuh@hack.bg>
2023-06-15 15:25:09 +0300

    Update Fadroma and secret-toolkit to the latest versions.

--- run.sh
+++ run.sh
@@ -2,18 +2,18 @@
 
 export NODE_OPTIONS=--openssl-legacy-provider
 
-function build {
-    if ! RUSTFLAGS='-C link-arg=-s' cargo +nightly-2023-02-07 build -p $1 --release --target wasm32-unknown-unknown ; then
-        exit
-    fi
-
+function optimize_wasm {
     cp ./target/wasm32-unknown-unknown/release/$2.wasm ./$1.wasm
     cat ./$1.wasm | gzip -9 > ./$1.wasm.gz
     rm ./$1.wasm
 }
 
-build fadroma-bench fadroma_bench
-build toolkit-bench toolkit_bench
+if ! RUSTFLAGS='-C link-arg=-s' cargo +nightly-2023-02-07 build --release --target wasm32-unknown-unknown ; then
+    exit
+fi
+
+optimize_wasm fadroma-bench fadroma_bench
+optimize_wasm toolkit-bench toolkit_bench
 
 if ! npx tsc ; then
     exit
