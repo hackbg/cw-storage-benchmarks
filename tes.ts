240f893148b5cf1e0a60af88eb68f5630d278397
Milen Radkov <milen@hack.bg>
2023-08-05 15:30:38 +0300

    fix: typo in import tes.ts

--- tes.ts
+++ tes.ts
@@ -1,5 +1,5 @@
 import * as assert from 'node:assert'
 import CwStorageBenchmarks from './api'
-import { getDeployment } from '@hackbg/fadroma
+import { getDeployment } from '@hackbg/fadroma'
 const deployment = await getDeployment(CwStorageBenchmarks).deploy()
 // add your assertions here
